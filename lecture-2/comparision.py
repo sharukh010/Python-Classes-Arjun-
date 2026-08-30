49666584fb1521b2dc3ffc16bca7e6e09c55a622
Pathan Sharukh Khan <145139884+sharukh010@users.noreply.github.com>
2026-08-30 20:43:28 +0530

    Add files via upload

---
+++ lecture-2/comparision.py
@@ -0,0 +1,22 @@
+"""
+comparision operators:
+all the comparision operators will return boolean 
+value as result (True / false)
+if the condition is correct you will get True 
+or else you will get False as result 
+
+1. equal to (==)
+2. not equal to (!=) 
+3. greater than (>)
+4. less than (<)
+5. greater than or equal (>=) 
+6. less than or equal to (<=)
+"""
+a = 10 
+b = 20 
+print(a == b) # False 
+print(a > b) #False 
+print(a < b) #True 
+print(a >= b) #False 
+print(a <= b) #True (if it is < or == )
+print(a != b) #True 
---
+++ lecture-2/conditional.py
@@ -0,0 +1,34 @@
+"""
+A number as input and we are going to say 
+if it is even number or odd number. 
+Algorithm: (sequence of steps written in plain english text)
+1. take the number as input from the  user 
+2. check if it is even 
+ - if yes we will print even 
+ - if no we will print odd 
+
+algorithms -> write the code in any programming language
+syntax -> grammar of programming language 
+ex: 
+1. my name is sharukh (correct) (it is correct grammar)
+2. name my is sharukh (wrong) its wrong grammar (syntax)
+
+variable = value (correct)
+value = variable (not correct) not following python syntax
+
+syntax of if statment: 
+if condition: <- : specify that after this a new code block 
+is going to start i.e there are bunch of lines that comes under 
+if condition 
+after : we can have code inside if condition and 
+code outside if condition 
+so to differentiate we use indentation (spaces before lines)
+    line -1 (belong to if)
+    line -2 (belong to if)
+    line -3 (belong to if)
+line - 4 (doesn't belong to if)
+
+to understand how to write conditions we need to learn 
+about comparision operators 
+"""
+number = int(input("Enter a number: ")) # taking number as input 
