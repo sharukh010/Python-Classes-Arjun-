33c9529e45970f4127d23bda3e47298439c3ff8e
Pathan Sharukh Khan <145139884+sharukh010@users.noreply.github.com>
2026-09-13 20:23:40 +0530

    Add files via upload

---
+++ Lecture-6/reverse_a_word.py
@@ -0,0 +1,5 @@
+word = input()
+# for index in range(len(word)-1,-1,-1):
+#     # range will generate all the numbers from n-1 to 0 from last to first 
+#     print(word[index],end="")
+# print(word[::-1])
---
+++ Lecture-6/string_slicing.py
@@ -0,0 +1,13 @@
+name = "Arjun"
+# print(name[])
+# name[start:end:step]
+# start -> from where you need to need to start 
+# end -> at which index you need to stop 
+# step -> how to go to next element by default it is 1 
+# name[0:3] -> Arj
+# print(name[0:3])
+# print(name[:3]) # by default start is 0 
+# print(name[0:]) # by default end is len(word)
+# print(name[:])
+# print(name[::2])
+print(name[::-1])
